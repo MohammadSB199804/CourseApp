d9ea9ad55b39b34e19f60f57bd3adfcf7f6b5e5b
MohammadSB199804 <mrl199804@gmail.com>
2021-10-17 11:35:30 +0300

    Add Angular Section

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'The Dating App';
+
+  constructor(private http:HttpClient){ // Angular comes with lifecycle events , the lifecycle event that takes place after the constructor known as initilization 
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
+   
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
-@NgModule({
+// this is a root module 
+
+@NgModule({ // angular module declares the components that are avaliable inside our application
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [ // for import angular modules into other modules and make use of them in our app  
+    BrowserModule, // for creating single page app .
+    AppRoutingModule ,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
